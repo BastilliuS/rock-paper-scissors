8fb09a2eadc17a210ba8dbf40d54e95ed0f0d258
BastilliuS <cristipetre48@gmail.com>
2023-01-24 23:03:21 +0200

    Beginning of play round function

--- script.js
+++ script.js
@@ -11,4 +11,47 @@ function getComputerChoice(){
          else return c;
     
 }
+function playRound(playerSelection,computerSelection){
+     computerSelection =  getComputerChoice();
+     let msg="";
+    if (playerSelection.toUpperCase()==="ROCK"){
+        if (computerSelection==="Rock"){
+            msg="Tie";
+        }
+        else if(computerSelection==="Paper"){
+            msg="You lose";
+        }
+        else if(computerSelection==="Scissors"){
+            msg="You win";
+        }
+    }
+    else if (playerSelection.toUpperCase()==="PAPER"){
+        if (computerSelection==="Paper"){
+            msg="Tie";
+        }
+        else if(computerSelection==="Scissors"){
+            msg="You lose";
+        }
+        else if(computerSelection==="Rock"){
+            msg="You win";
+        }
+    }
+    else if (playerSelection.toUpperCase()==="SCISSORS"){
+        if (computerSelection==="Scissors"){
+            msg="Tie";
+        }
+        else if(computerSelection==="Rock"){
+            msg="You lose";
+        }
+        else if(computerSelection==="Paper"){
+            msg="You win";
+        }
+    }
+    return msg;
+}
+    
+
 console.log(getComputerChoice());
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
