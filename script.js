939c839aa83cf2d64d21b25d86f152fa3febbe92
BastilliuS <cristipetre48@gmail.com>
2023-02-04 14:34:40 +0200

    Commit before pause/nap

--- script.js
+++ script.js
@@ -57,15 +57,15 @@ function playRound(choice){
 }
 let p=0;
 let c=0;
- const rock=document.createElement("button");
- rock.textContent="Rock";
- const paper=document.createElement("button");
- paper.textContent="Paper";
- const scissors=document.createElement("button");
- scissors.textContent="Scissors";
- const container=document.querySelector("#container");
- container.append(rock,paper,scissors);
- const results = document.querySelector("#results");
+const rock=document.createElement("button");
+rock.textContent="Rock";
+const paper=document.createElement("button");
+paper.textContent="Paper";
+const scissors=document.createElement("button");
+scissors.textContent="Scissors";
+const container=document.querySelector("#container");
+container.append(rock,paper,scissors);
+const results = document.querySelector("#results");
  rock.addEventListener("click",()=>{
     roundResult=playRound("rock");
     results.textContent="";
@@ -76,19 +76,18 @@ let c=0;
     player.textContent+=p;
     results.innerHTML+=roundResult;
     results.innerHTML+="<br>";
- })
+})
  paper.addEventListener("click",()=>{
     roundResult=playRound("paper");
     results.textContent="";
     computer.textContent="";
-    computer.style.cssText="margin=100px";
     computer.textContent="Computer:";
     player.textContent="Player:";
     computer.textContent+=c;
     player.textContent+=p;
     results.innerHTML+=roundResult;
     results.innerHTML+="<br>";
- })
+})
  scissors.addEventListener("click",()=>{
     roundResult=playRound("scissors");
     results.textContent="";
@@ -99,13 +98,24 @@ let c=0;
     player.textContent+=p;
     results.innerHTML+=roundResult;
     results.innerHTML+="<br>";
- })
- const score=document.querySelector("#score");
- const computer=document.createElement("div");
- computer.style.cssText="margin=100px;";
- const player=document.createElement("div");
- const body=document.querySelector("body");
- body.style.cssText="display: flex; align-items: center; justify-content: space-around; flex-direction: column;"
+})
 
- score.append(computer,player);
- console.log(container);
+rock.style.margin="10px";
+paper.style.margin="10px";
+scissors.style.margin="10px";
+const score=document.querySelector("#score");
+const computer=document.createElement("div");
+computer.style.margin="10px";
+
+const player=document.createElement("div");
+player.style.margin="10px";
+const body=document.querySelector("body");
+
+const bigContainer=document.querySelector("#bigContainer");
+bigContainer.append(container,results,score);
+document.getElementById("bigContainer").style.display="flex";
+document.getElementById("bigContainer").style.flexDirection="column";
+document.getElementById("bigContainer").style.justifyContent="space-between";
+document.getElementById("bigContainer").style.alignItems="center";
+score.append(computer,player);
+console.log(container);
